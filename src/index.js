a24788996e6be24e3f8b2f7974d6658374ac258b
Sebastian Loucim <sebastian.loucim@gmail.com>
2022-05-29 18:22:24 -0300

    CardList component and App component added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card.js'
 import 'tachyons';
-import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
+//------------------\\
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <App/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
