157400bf31ebfda7820da7bf72363905fe95b34b
loucims <sebastian.loucim@gmail.com>
2022-06-12 13:39:23 -0300

    Final hooks refactoring

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from '../Components/SearchBox';
 import Scroll from "../Components/Scroll";
 
-class App extends React.Component {
+/*class App extends React.Component {
     constructor(){
         super()
         this.state = {
@@ -49,5 +49,47 @@ class App extends React.Component {
             
         );
     }
+}*/
+const App = () =>{
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState("");
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => {response.json()})
+        .then((users) => {setRobots(users)});
+    }, []);
+
+    onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    }
+    
+
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+
+    if (!robots.length){ 
+        return (
+            <div>
+                <h1 className="tc white f1">Loading...</h1>
+            </div>)
+    }
+
+    return (
+        <div className='tc'>
+            <h1 className="white f1">RoboFriends</h1>
+            <div>
+                <h3 className="white" style={{marginTop: '-20px'}}>Made by: Sebastian Loucim</h3>
+            </div>
+            <SearchBox searchChange={onSearchChange}/>
+            <hr/>
+            <Scroll>
+                <CardList robots={filteredRobots}/>
+            </Scroll>
+        </div>
+        
+    );
 }
 export default App;
